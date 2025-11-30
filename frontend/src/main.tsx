e17169c2abc55fb5e6a6deece9fe57598d4acb72
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-11-30 13:41:28 +0700

    feat(fe/axios): enhance error handling with custom ApiError class and integrate sonner for notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, type ErrorInfo } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
+import { Toaster } from 'sonner'
 import App from './App.tsx'
 import ErrorFallback from './components/common/ErrorFallback.tsx'
 import { ThemeProvider } from './providers/ThemeProvider.tsx'
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
         }}
       >
         <App />
+        <Toaster />
       </ErrorBoundary>
     </ThemeProvider>
   </StrictMode>,
