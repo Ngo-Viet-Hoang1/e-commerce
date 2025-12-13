34686a1809f55159999613ccc186f560206e16d8
Nguyen Dac Dien <diennd1910@gmail.com>
2025-12-13 21:59:17 +0700

    implement oders model, routes, controller, service, and repository with CRUD operations

--- backend/src/api/v1/routes/index.ts
+++ backend/src/api/v1/routes/index.ts
@@ -3,6 +3,9 @@ import authRoute from '../modules/auth/auth.route.js'
 import userRoute from '../modules/user/user.route.js'
 import errorRoute from './error.route.js'
 import homeRoute from './home.route.js'
+import orderRouter from '../modules/order/order.route'
+import attributeRoute from '../modules/attributes/attribute.route'
+import attributeValueRoute from '../modules/attributeValue/attributeValue.route'
 
 const router = Router()
 
@@ -10,5 +13,8 @@ router.use('/', homeRoute)
 router.use('/auth', authRoute)
 router.use('/users', userRoute)
 router.use('/errors', errorRoute)
+router.use('/orders', orderRouter)
+router.use('/attributes', attributeRoute)
+router.use('/attributes', attributeValueRoute)
 
 export default router
