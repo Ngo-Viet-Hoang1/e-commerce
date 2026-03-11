260d996b040fe87b3a9110a1a3b87da3630b3ac8
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2026-03-11 20:57:20 +0700

    style(order-items): standardize naming conventions and improve response messages in controller and service

--- backend/src/api/v1/modules/order-items/order-items.controller.ts
+++ backend/src/api/v1/modules/order-items/order-items.controller.ts
@@ -4,7 +4,8 @@ import {
   type CreateOrderItemBody,
   type ListOrderItemsQuery,
   type OrderItemIdParam,
-  type updateOrderItemBody,
+  type UpdateOrderItemBody,
+  type UpdateOrderItemVariantBody,
 } from './order-items.schema'
 import { orderItemService } from './order-items.service'
 
@@ -27,37 +28,33 @@ class OrderItemController {
 
     const orderItem = await orderItemService.findById(id)
 
-    SuccessResponse.send(
-      res,
-      orderItem,
-      'Attribute Value retrieved successfully',
-    )
+    SuccessResponse.send(res, orderItem, 'Order Item retrieved successfully')
   }
 
   create = async (req: Request, res: Response) => {
     const data = req.validatedData?.body as CreateOrderItemBody
 
-    const createditem = await orderItemService.create(data)
+    const createdItem = await orderItemService.create(data)
 
-    SuccessResponse.created(res, createditem, 'Order Item created successfully')
+    SuccessResponse.created(res, createdItem, 'Order Item created successfully')
   }
 
   deleteById = async (req: Request, res: Response) => {
     const { id } = req.validatedData?.params as OrderItemIdParam
 
-    const deleteditem = await orderItemService.deleteById(id)
+    const deletedItem = await orderItemService.deleteById(id)
 
-    SuccessResponse.send(res, deleteditem, 'Order Item deleted permanently')
+    SuccessResponse.send(res, deletedItem, 'Order Item deleted permanently')
   }
 
   softDeleteById = async (req: Request, res: Response) => {
     const { id } = req.validatedData?.params as OrderItemIdParam
 
-    const deleteditem = await orderItemService.softDeleteById(id)
+    const deletedItem = await orderItemService.softDeleteById(id)
 
     SuccessResponse.send(
       res,
-      deleteditem,
+      deletedItem,
       'Order Item soft deleted successfully',
     )
   }
@@ -65,23 +62,23 @@ class OrderItemController {
   restore = async (req: Request, res: Response) => {
     const { id } = req.validatedData?.params as OrderItemIdParam
 
-    const restoreditem = await orderItemService.restoreById(id)
+    const restoredItem = await orderItemService.restoreById(id)
 
-    SuccessResponse.send(res, restoreditem, 'Order Item restored successfully')
+    SuccessResponse.send(res, restoredItem, 'Order Item restored successfully')
   }
 
   updateById = async (req: Request, res: Response) => {
     const { id } = req.validatedData?.params as OrderItemIdParam
-    const data = req.validatedData?.body as updateOrderItemBody
+    const data = req.validatedData?.body as UpdateOrderItemBody
 
-    const updateditem = await orderItemService.updateById(id, data)
+    const updatedItem = await orderItemService.updateById(id, data)
 
-    SuccessResponse.send(res, updateditem, 'Order Item updated successfully')
+    SuccessResponse.send(res, updatedItem, 'Order Item updated successfully')
   }
 
   updateItemVariant = async (req: Request, res: Response) => {
     const { id } = req.validatedData?.params as OrderItemIdParam
-    const { variantId } = req.validatedData?.body as { variantId: number }
+    const { variantId } = req.validatedData?.body as UpdateOrderItemVariantBody
 
     const updatedItem = await orderItemService.updateOrderItemVariant(
       id,
